c530a751d7b5afb3884a997fe2a3f3cff1779db1
HMETAO <1430271732@qq.com>
2024-02-17 13:30:36 +0800

    init

---
+++ app/src/main/java/com/hmetao/ticketunion/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hmetao.ticketunion.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hmetao.ticketunion.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
